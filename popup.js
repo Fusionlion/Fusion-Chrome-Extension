16d9c3a0e227dab92c6ccccf9bd60db12a7e1539
Zablon Charles <zablon.charles@gmail.com>
2023-08-29 23:38:17 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", function () {
+  loadpagedata();
+});
+
+function loadpagedata() {
+  getQuote();
+  // Array of image URLs
+  const backgroundImages = ["digideer.jpeg", "fox.jpg"];
+
+  // Function to set a random background image
+
+  // Get a random index from the array
+  const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+
+  // Get the selected image URL
+  const selectedImage = backgroundImages[randomIndex];
+
+  // Set the body background to the selected image
+  document.body.style.backgroundImage = `url(backgrounds/${selectedImage})`;
+}
+
+function getQuote() {
+  const quotes = [
+    "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. - Steve Jobs",
+    "In the middle of every difficulty lies opportunity. In the face of adversity, we discover our true strengths. - Albert Einstein",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. Persistence and determination are the keys to achieving greatness. - Winston Churchill",
+    "Your time is limited, don't waste it living someone else's life. Follow your passions, chase your dreams, and create your own path. - Steve Jobs",
+    "The only person you are destined to become is the person you decide to be. Your choices shape your destiny. - Ralph Waldo Emerson",
+    "The biggest risk is not taking any risk. In a world that is constantly changing, the only strategy that is guaranteed to fail is not taking risks. - Mark Zuckerberg",
+    "Believe you can and you're halfway there. Confidence and self-belief are the driving forces behind all achievements. - Theodore Roosevelt",
+    "Life is 10% what happens to us and 90% how we react to it. Our attitude and mindset determine our happiness and success. - Charles R. Swindoll",
+    "The only limit to our realization of tomorrow will be our doubts of today. Embrace your potential and have faith in your abilities. - Franklin D. Roosevelt",
+    "The best way to predict the future is to create it. Your actions today shape the world you'll live in tomorrow. - Abraham Lincoln",
+    "Success is not just about making money. It's about making a difference. - Unknown",
+    "The harder you work for something, the greater you'll feel when you achieve it. - Unknown",
+    "Your journey begins with a single step. Don't be afraid to take it. - Unknown",
+    "Your dreams are worth chasing. Never give up on what sets your soul on fire. - Unknown",
+    "Don't be discouraged by the challenges you face; they are just stepping stones to success. - Unknown",
+    "Every day is a new opportunity to make a positive change in your life. - Unknown",
+    "Your attitude determines your direction. Stay positive and keep moving forward. - Unknown",
+    "The only way to do great work is to love what you do. - Unknown",
+    "Your potential is endless. Don't limit yourself. - Unknown",
+    "Success is not the destination; it's the journey. - Unknown",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  document.getElementById("quote").innerHTML = randomQuote;
+}
+
+function constanttime() {
+  // Get the current date
+  var today = new Date();
+  var hours = today.getHours() % 12 || 12;
+  //const currentDate = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+  currentDate = hours + ":" + today.getMinutes();
+
+  // Display the date
+  document.getElementById("current-date").innerHTML = currentDate;
+
+  // Create a new Date object for April 23, 2023
+  const date = new Date();
+
+  // Define arrays for day names and month names
+  const dayNames = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ];
+  const monthNames = [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER",
+  ];
+
+  // Get the day, month, and year
+  const day = dayNames[date.getUTCDay()];
+  const month = monthNames[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+
+  // Get the day of the month with leading zeros (e.g., 01, 02, 03)
+  const dayOfMonth = String(date.getUTCDate()).padStart(2, "0");
+
+  // Format the date
+  const formattedDate = `${day}, ${month} / ${dayOfMonth} / ${year}`;
+
+  document.getElementById("current-full-date").innerHTML = formattedDate;
+}
---
+++ tapped.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.create({ url: "main.html" }); // Replace with your desired URL
+  });
